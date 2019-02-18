a106921329e0e5bedfab1fa265d6b7760c6fab6c
davschne <davschne@gmail.com>
2019-02-18 14:16:07 -0800

    Add #include guard in width.h.

--- ch05/width.h
+++ ch05/width.h
@@ -1,7 +1,12 @@
 // 5.8.1/92
 // Compute the width of a multi-line message (i.e. the max string length)
 
+#ifndef GUARD_width_h
+#define GUARD_width_h
+
 #include <string>
 #include <vector>
 
 std::string::size_type width(const std::vector<std::string>&);
+
+#endif
