84c54d4ac45ee441c26b6e2d24b7ebe789a1f7a9
David E. Schneider <davschne@gmail.com>
2019-06-15 15:32:45 -0700

    Convert median to template function.

--- lib/median.h
+++ lib/median.h
@@ -1,8 +1,25 @@
-#ifndef GUARD_median_h
-#define GUARD_median_h
+#ifndef MEDIAN_H
+#define MEDIAN_H
 
+#include <algorithm>
+#include <stdexcept>
 #include <vector>
 
-double median(std::vector<double>);
+template <class T>
+T median(std::vector<T> v) {
+    typedef typename std::vector<T>::size_type vec_sz;
+
+    vec_sz size = v.size();
+    if (size == 0)
+        throw std::domain_error("median of an empty vector");
+    
+    std::sort(v.begin(), v.end());
+
+    vec_sz mid = size / 2;
+
+    return size % 2 == 0
+        ? (v[mid] + v[mid + 1]) / 2
+        : v[mid];
+}
 
 #endif
